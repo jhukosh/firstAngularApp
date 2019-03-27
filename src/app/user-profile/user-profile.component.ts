932c10fb24d280889efecf5287bf59d60b211651
Julia Hu <juliahukoshi@gmail.com>
2019-03-27 15:51:23 +0100

    finished angular 02 quest

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+
+  ageDisplayed = false;
+  buttonText = "Afficher l'âge";
+
+User = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
+  displayAge(){
+    if (this.ageDisplayed) {
+      this.ageDisplayed = false;
+      this.buttonText = "Afficher l'âge";
+    } else {
+      this.ageDisplayed = true;
+      this.buttonText = "Cacher l'âge";
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
